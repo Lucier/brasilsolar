c8fcb32c34b910ddc65fb8c901f5071ad916b157
Lucier F Lima <lucierflima@gmail.com>
2018-10-16 22:56:57 -0300

    alterar inclusao dj-static

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -205,9 +205,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 MEDIA_ROOT = 'media'
 MEDIA_URL = '/media/'
